ff31b704f73d82b3f97c373c6d9c2d88be4f3f67
BenduOlo253 <jmanuerb1@outlook.com>
2023-08-24 08:06:17 -0600

    Practica 1

--- _p15_ConvertirADolares.java
+++ _p15_ConvertirADolares.java
@@ -1,5 +1,5 @@
 //Alumno: Jose Manuel Roberto Badillo.
-//Descripción: Crear un programa que convierta una determinada cantidad de dolares estadounidenses a pesos mexicanos.
+//Descripción: Crear un programa que convierta una determinada cantidad de pesos mexicanod a dolares estadounidenses.
 
 import java.util.Scanner;
 public class _p15_ConvertirADolares{
@@ -8,20 +8,19 @@ public class _p15_ConvertirADolares{
         System.out.print("\033[H\033[2J"); System.out.flush();
 
         //Declaramos variables.
-        float dlrs;
-        double psmxn;
+        double psmxn, dlrs;
         //Creamos una entrada de datos.
         Scanner DatoIng = new Scanner(System.in);
 
         //Obtenemos los datos.
-        System.out.println("Este programa convierte una cantidad determinada de dolares estadounidenses a pesos mexicanos.\nPresione ENTER para continuar");
+        System.out.println("Este programa convierte una cantidad determinada de pesos mexicanos a dolares estadounidenses.\nPresione ENTER para continuar");
         DatoIng.nextLine();
-        System.out.println("Digite la cantidad de dolares: "); dlrs = DatoIng.nextFloat();
+        System.out.println("Digite la cantidad de pesos: "); psmxn = DatoIng.nextFloat();
         
         //Hacemos la conversion.
-        psmxn = dlrs*16.81;
+        dlrs = psmxn/16.18;
 
         //Mostramos resultados en pantalla.
-        System.out.printf("La cantidad de %.2f dolares estadounidenses equivale a %.2f pesos mexicanos.",dlrs, psmxn);
+        System.out.printf("La cantidad de %.2f pesos mexicanos equivale a %.2f dolares estadounidenses.", psmxn, dlrs);
     }
 }
