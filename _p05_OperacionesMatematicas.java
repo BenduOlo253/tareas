0303195b87962472d501ccc290bfe274893d5cac
BenduOlo253 <jmanuerb1@outlook.com>
2023-08-23 21:58:59 -0600

    tareas

--- _p05_OperacionesMatematicas.java
+++ _p05_OperacionesMatematicas.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class _p05_OperacionesMatematicas{
     public static void main(String[] args) {
         double pot;
-        float Var1, Var2, Results, suma, resta, multi, div, res;
+        float Var1, Var2, suma, resta, multi, div, res;
         Scanner datoIng = new Scanner(System.in);
         System.out.println("Este programa, calcula todas las operaciones basicas.");
         //Obtencion de los datos.
@@ -21,7 +21,7 @@ public class _p05_OperacionesMatematicas{
         System.out.println(String.format("El resultado de la suma entre %.2f, y %.2f es: %.2f.", Var1, Var2, suma));
         System.out.println(String.format("El resultado de la resta entre %.2f, y %.2f es: %.2f.", Var1, Var2, resta));
         System.out.println(String.format("El resultado de la multiplicación entre %.2f, y %.2f es: %.2f.", Var1, Var2, multi));
-        System.out.println(String.format("El resultado de la división entre %.2f, y %.2f es: %.2f.\n Y el residuo es %.2f ", Var1, Var2, suma, res));
+        System.out.println(String.format("El resultado de la división entre %.2f, y %.2f es: %.2f.\n Y el residuo es %.2f ", Var1, Var2, div, res));
         System.out.println(String.format("El resultado de %.2f elevado a %.2f es: %.2f", Var1, Var2, pot));
 
     }
---
+++ _p14_CalculoTiempo.java
@@ -0,0 +1,13 @@
+//Alumno: Jose Manuel Roberto Badillo.
+//Descripcion: Crear un prigrama que dado un determinado numero de horas, calcular su equivalente en días, minutos y segundos.
+
+import java.util.Scanner;
+public class _p14_CalculoTiempo {
+    public static void main(String[] args) {
+        //Limpiamos pantalla de consola.
+        System.out.print("\033[H\033[2J"); System.out.flush();
+
+        // Declaramos variables.
+        
+    }
+}
