ea98a08e6884e8efd961dd8814fceca5a11d9677
BenduOlo253 <jmanuerb1@outlook.com>
2023-08-23 21:45:33 -0600

    Actividad 2

--- _p08_FuncionesTrigonometrigas.java
+++ _p08_FuncionesTrigonometrigas.java
@@ -4,9 +4,20 @@
 import java.util.Scanner;
 public class _p08_FuncionesTrigonometrigas {
     public static void main(String[] args) {
-        // limpiamos la pantalla de consola.
+        //Se limpia la pantalla de la consola.
         System.out.print("\033[H\033[2J"); System.out.flush();
-        
 
+        //Obtencion de datos.
+        System.out.print("Dame un angulo en Grados: ");
+        double angulog = new Scanner(System.in).nextDouble();
+
+        //Calculos.
+        double angulor = Math.toRadians(angulog);
+        double sen = Math.sin(angulor);
+        double cos = Math.cos(angulor);
+        double tan = Math.tan(angulor);
+        
+        //Mostramos resultados en pantalla.
+        System.out.printf("El angulo es: %.2f, Seno: %.2f, Coseno: %.2f, Tangente:%.2f",angulor,sen,cos,tan);
     }
 }
---
+++ _p09_Dividir_En_Cifras.java
@@ -0,0 +1,28 @@
+//Alumno: Jose Manuel Roberto Badillo.
+//Descripcion: Crear un programa que logre dividir un numero de 3 cifras en centenas, decimas y unidades.
+
+import java.util.Scanner;
+public class _p09_Dividir_En_Cifras{
+    public static void main(String[] args) {
+        //Limpiamos pantalla de la consola.
+        System.out.print("\033[H\033[2J"); System.out.flush();
+
+        //Declaramos variables.
+        int units, cent, dec, num;
+
+        //Creamos la entrada de datos.
+        Scanner DatoIng = new Scanner(System.in);
+
+        //Obtencion de datos.
+        System.out.println("Dame un numero entero de 3 cifras: ");
+        num = DatoIng.nextInt();
+        cent = num / 100 ;
+        dec = ( num - cent * 100) / 10 ;
+        units = ( num - (cent * 100 + dec * 10) );
+        System.out.println("El nuemero introducido fue : " + num);
+        System.out.println("Centenas : " + cent);
+        System.out.println("Decenas : " + dec);
+        System.out.println("Unidades : " + units);
+
+    }
+}
