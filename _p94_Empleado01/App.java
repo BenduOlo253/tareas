aba545eb7a58bd75e80876f2cbe84357a7b5e3e2
BenduOlo253 <jmanuerb1@outlook.com>
2023-10-20 17:55:43 -0600

    Actividad 15

---
+++ _p94_Empleado01/App.java
@@ -0,0 +1,18 @@
+package _p94_Empleado01;
+
+public class App {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        empleado1.Nombre = "Juan Perez";
+        empleado1.Edad = 35;
+        System.out.println(empleado1.toString());
+
+        Empleado empleado2 = new Empleado();
+        empleado2.Nombre = "Maria de jesus";
+        empleado2.Edad = 25;
+        System.out.println(empleado2.toString());
+
+        System.out.println("nombre: " + empleado1.Nombre);
+        System.out.println("Edad: " + empleado1.Edad);
+    }
+}
---
+++ _p95_Empleado02/App.java
@@ -0,0 +1,12 @@
+package _p95_Empleado02;
+
+public class App {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        empleado1.setNombre("Juan Perez");
+        empleado1.setEdad(35);
+        System.out.println(empleado1.toString());
+        System.out.println("Nombre: " + empleado1.getNombre());
+        System.out.println("Edad: " + empleado1.getEdad());
+    }
+}
